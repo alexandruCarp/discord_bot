f33e180970ebae32c291a32858edc7c0a96d44e0
Alexandru Carp <alexandrucarp02@gmail.com>
2021-12-08 18:41:36 +0200

    Added copyright notice
    Signed-off-by: Alexandru Carp <alexandrucarp02@gmail.com>

--- bot-discord.py
+++ bot-discord.py
@@ -1,7 +1,19 @@
+#This file is part of bot-discord.
+#
+#    bot-discord is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    bot-discrod is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with bot-discord.  If not, see <https://www.gnu.org/licenses/>
 
-    
-
-    #!./.venv/bin/python
+#!./.venv/bin/python
 
 import discord      # base discord module
 import code         # code.interact
